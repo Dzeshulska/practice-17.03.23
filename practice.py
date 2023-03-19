02db223412e29bf94535f340e0b459ce8d4cd05f
Alla_Dseshulska <dzeshulska@gmail.com>
2023-03-19 10:59:38 +0200

    added practice

---
+++ practice.py
@@ -0,0 +1,33 @@
+def greeting(fn, text = "") :
+    print(f"Hello{fn}")
+    print(f"{text}")
+
+def user({"name", "money","bag"})
+
+is running = True
+while is running :
+    user_choose = input("""
+        a) category([])
+        b)show all product
+        c)Admin mode
+        d)quit
+    """)
+    if user_choose == a :
+        show_
+    
+
+
+
+def store(label,price,desc) :
+    return {
+        "label" : label,
+        "price" : price,
+        "desc" : desc
+    }
+
+
+
+
+
+
+
